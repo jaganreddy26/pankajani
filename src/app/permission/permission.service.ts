08b6cd56b31e13716990adbe290a79bb61368dbe
ranga <reddy.ranga1234@gmail.com>
2018-09-17 14:46:20 +0530

    Partiailly SeekPermission Implemented

--- src/app/permission/permission.service.ts
+++ src/app/permission/permission.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class PermissionService {
+  BusinessId:any =3;
+  private host = environment.API_END_POINT;
+  private url: string = '';
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  GetStatus(data){
+    this.url =this.host+'/api/UbtApi/GetStatus';
+    return this.http.post(this.url,data);
+  }
+  getCustomerName()
+  {
+    var data = {BusinessId:this.BusinessId } ;
+    this.url = this.host+'/api/UbtApi/GetCustomers';
+    return this.http.post(this.url,data)
+  }
+  getUbtIds(data)
+  {
+    // var data = {BusinessId:this.BusinessId } ;
+    this.url = this.host+'/api/UbtApi/GetUBT';
+    return this.http.post(this.url,data)
+  }
+  getSeekPermissionDetailsByPoId(data){
+    this.url=this.host+'/api/UbtApi/GetSeekPermissionSelection';
+    return this.http.post(this.url,data);
+  }
 }
