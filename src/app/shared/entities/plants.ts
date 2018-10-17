98390f3d85e914667f96a9a7cac11315b9217ba6
jaganreddy26 <jagannath.sotupalli@gmail.com>
2018-10-17 08:59:01 +0530

    commit

---
+++ src/app/shared/entities/plants.ts
@@ -0,0 +1,15 @@
+export class Plants {
+    PlantName:string;
+    Email :any;
+    AlternativeEmail:any;
+    Mobile:any;
+    AlternativeMobile:any;
+    Address1:any;
+    Address2:any;
+    Address3:any;
+    CIN:any;
+    GSTIN:any;
+    PAN:any;
+    TAN_NO:any;
+    BusinessId:any;
+}
