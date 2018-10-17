e0a36243fc07c8f70c96b1f216377e814082ff88
jaganreddy26 <jagannath.sotupalli@gmail.com>
2018-10-17 09:08:13 +0530

    commit

--- src/app/MasterData/plantdetails/plantdetails.component.ts
+++ src/app/MasterData/plantdetails/plantdetails.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit,TemplateRef } from '@angular/core';
+import { Plants } from '../../shared/entities/plants';
+
 
 @Component({
   selector: 'app-plantdetails',
@@ -6,12 +8,14 @@ import { Component, OnInit,TemplateRef } from '@angular/core';
   styleUrls: ['./plantdetails.component.css']
 })
 export class PlantdetailsComponent implements OnInit {
-
+plant:Plants = new Plants();
   constructor() { 
   
   }
 
   ngOnInit() {
   }
-
+  save(){
+    console.log(this.plant)
+  }
 }
